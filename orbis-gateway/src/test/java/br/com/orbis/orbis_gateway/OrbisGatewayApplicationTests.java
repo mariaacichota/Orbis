6397c2724afc018ae472da1e1dbf011a7e87316b
ArielCAlves <ariel@julioalves.adv.br>
2025-12-09 22:10:57 -0300

    esqueleto do gateway

---
+++ orbis-gateway/src/test/java/br/com/orbis/orbis_gateway/OrbisGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orbis.orbis_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbisGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
